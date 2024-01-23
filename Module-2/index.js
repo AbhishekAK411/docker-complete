7eecc395ec5cc14eabce04bc419f42e3218c350b
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-01-23 17:47:40 +0530

    updates

--- Module-2/index.js
+++ Module-2/index.js
@@ -8,7 +8,27 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get("/", (req,res) => {
-    
+    res.send(`
+        <html>
+            <head>
+                <link rel="stylesheet" href="styles.css">
+            </head>
+            <body>
+                <section>
+                    <h2>My course goal</h2>
+                    <h3>${userGoal}</h3>
+                </section>
+
+                <form action="/store-goal" method="POST">
+                    <div class="form-control">
+                        <label>Course Goal</label>
+                        <input type="text" name="goal" />
+                    </div>
+                    <button>Set Course Goal<button>
+                </form>
+            </body>
+        </html>
+    `)
 });
 
 app.post("/store-goal", (req,res) => {
